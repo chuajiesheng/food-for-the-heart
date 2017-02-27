6b67d57e0a6a4208034bdd3291ec656aac09dacc
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-02-28 00:48:42 +0800

    Ensure that login redirect user to login path

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -6,4 +6,10 @@ describe 'home page', :type => :feature do
     expect(page).to have_content 'Login'
     expect(page).to have_content 'Assign Meals'
   end
+
+  it 'login redirect me to devise login page' do
+    visit '/'
+    click_link 'Login'
+    expect(page).to have_current_path(new_user_session_path)
+  end
 end

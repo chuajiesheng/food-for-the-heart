d1f397d21f77f9467071f9f0ba7f7593f2e61271
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-02-28 01:50:50 +0800

    Expand feature testing to cover navbar

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -15,3 +15,30 @@ describe 'home page', :type => :feature do
     expect(page).to have_current_path(new_user_session_path)
   end
 end
+
+feature 'user login and assess features' do
+  background do
+    FactoryGirl.create(:user)
+  end
+
+  scenario 'navbar change according to user status' do
+    visit '/'
+    within('.navbar') do
+      click_link 'Login'
+    end
+    expect(page).to have_current_path(new_user_session_path)
+    within('#new_user') do
+      fill_in 'user_email', with: 'sample@email.com'
+      fill_in 'user_password', with: '!abcd1234'
+      click_button 'Log in'
+    end
+    expect(page).to have_content 'Meal Preference'
+    expect(page).to have_content 'Meal for the day'
+    expect(page).to have_content 'Signed in as sample@email.com'
+    expect(page).to have_content 'Logout'
+    within('.navbar') do
+      click_link 'Logout'
+    end
+    expect(page).to have_content 'Login'
+  end
+end

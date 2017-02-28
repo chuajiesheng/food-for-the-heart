2a9328f6b03391491a26423e75f6a79486267fda
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-02-28 23:53:20 +0800

    Add routing spec for user controller

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'routes', :type => :routing do
   it 'routes / to home#index' do
     expect(:get => '/').to route_to(:controller => 'home', :action => 'index')
   end
+
+  it 'routes /user/index to user#index' do
+    expect(:get => '/user/index').to route_to(:controller => 'user', :action => 'index')
+  end
 end

9a88383d1aafa1576705178fe2f2b1dbd017fab7
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 00:47:44 +0800

    Add schema changes to preferences

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227162816) do
+ActiveRecord::Schema.define(version: 20170228162357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "preferences", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "chicken"
+    t.boolean  "mutton"
+    t.boolean  "beef"
+    t.boolean  "fish"
+    t.boolean  "prawn"
+    t.boolean  "squid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_preferences_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

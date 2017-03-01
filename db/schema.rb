fbfd73c77f187ae0d75407c839ed3d87d96fb8a8
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 18:37:14 +0800

    Add schema changes for meal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228162357) do
+ActiveRecord::Schema.define(version: 20170301102356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meals", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "meal_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_meals_on_user_id", using: :btree
+  end
 
   create_table "preferences", force: :cascade do |t|
     t.integer  "user_id"
@@ -45,5 +53,6 @@ ActiveRecord::Schema.define(version: 20170228162357) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "meals", "users"
   add_foreign_key "preferences", "users"
 end

553ca96af977fbb0d936389931884a2c83536add
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 19:15:52 +0800

    Meal availabilty is set zero at the start. Also implemented total function

--- app/models/meal_availability.rb
+++ app/models/meal_availability.rb
@@ -1,5 +1,16 @@
 class MealAvailability < ApplicationRecord
+  before_save :default_value
+
+  def default_value
+    self.chicken ||= 0
+    self.mutton ||= 0
+    self.beef ||= 0
+    self.fish ||= 0
+    self.prawn ||= 0
+    self.squid ||= 0
+  end
+
   def total
-    10
+    chicken + mutton + beef + fish + prawn + squid
   end
 end
--- spec/models/meal_availability_spec.rb
+++ spec/models/meal_availability_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe MealAvailability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#total' do
+    let(:meal) { FactoryGirl.create(:meal_availability) }
+
+    it 'return the total across all the meals' do
+      expect(meal.total).to eq(6)
+    end
+  end
+
+  describe 'before_create' do
+    it 'set chicken meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.chicken).to eq(0)
+    end
+
+    it 'set mutton meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.mutton).to eq(0)
+    end
+
+    it 'set beef meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.beef).to eq(0)
+    end
+
+    it 'set fish meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.fish).to eq(0)
+    end
+
+    it 'set prawn meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.prawn).to eq(0)
+    end
+
+    it 'set squid meal type to be zero' do
+      meal_availability = MealAvailability.create!
+      expect(meal_availability.squid).to eq(0)
+    end
+
+  end
 end

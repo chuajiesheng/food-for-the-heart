e928b04f48eb3e1a49eb5f8a49ad14ecf58888b5
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 20:19:32 +0800

    Add helper method to generate array of preference

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,16 @@
 class Preference < ApplicationRecord
   belongs_to :user
+
+  def to_array
+    preferences = []
+
+    preferences << 'chicken' if self.chicken?
+    preferences << 'mutton' if self.mutton?
+    preferences << 'beef' if self.beef?
+    preferences << 'fish' if self.fish?
+    preferences << 'prawn' if self.prawn?
+    preferences << 'squid' if self.squid?
+
+    preferences
+  end
 end
--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Preference, type: :model do
   it { should belong_to :user }
+
+  describe '#to_array' do
+    it 'return an array of preference' do
+      user = FactoryGirl.create(:user)
+      preference = FactoryGirl.create(:preference, user: user, mutton: true, fish: true)
+
+      expect(preference.to_array).to eq(['mutton', 'fish'])
+    end
+  end
+
 end

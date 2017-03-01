1522df23f44a8329a06f23239f3ef0b264b60862
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 23:10:48 +0800

    Ensure user have the rights to access the page

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -1,4 +1,15 @@
 class MealController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_correct_user!
+
   def show
+  end
+
+  private
+
+  def check_correct_user!
+    unless current_user.id == params[:id].to_i
+      head(403)
+    end
   end
 end
--- spec/controllers/meal_controller_spec.rb
+++ spec/controllers/meal_controller_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe MealController, type: :controller do
-
   describe 'GET #show' do
-    it 'returns http success' do
-      get :show, params: { id: 1 }
-      expect(response).to have_http_status(:success)
+    describe 'user not signed in' do
+      it 'blocks unauthenticated access' do
+        get :show, params: { id: 1 }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe 'user is signed in' do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it 'returns http success' do
+        get :show, params: { id: user.id }
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'does not allow access another user preference' do
+        get :show, params: { id: 42 }
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
-
 end

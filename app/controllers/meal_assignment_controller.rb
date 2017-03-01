2e3993ae263c42495805811a93bf1bd98d38df02
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 21:17:43 +0800

    Expect assignment controller to call services object

--- app/controllers/meal_assignment_controller.rb
+++ app/controllers/meal_assignment_controller.rb
@@ -1,4 +1,6 @@
 class MealAssignmentController < ApplicationController
   def index
+    service = MealAssignmentService.new
+    service.generate_meal_availability(User.count)
   end
 end
--- spec/controllers/meal_assignment_controller_spec.rb
+++ spec/controllers/meal_assignment_controller_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe MealAssignmentController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    before do
+      FactoryGirl.create(:preference, user: FactoryGirl.create(:user))
+      FactoryGirl.create(:preference, user: FactoryGirl.create(:user, email: '1@email.com'))
+      FactoryGirl.create(:preference, user: FactoryGirl.create(:user, email: '2@email.com'))
+      FactoryGirl.create(:preference, user: FactoryGirl.create(:user, email: '3@email.com'))
+    end
+
+    it 'call MealAssignmentService to generate meal availability' do
+      expect_any_instance_of(MealAssignmentService).to receive(:generate_meal_availability).with(4)
+      get :index
     end
   end
 

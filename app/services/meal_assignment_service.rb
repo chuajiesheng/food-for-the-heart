a68c03295b442af40a3e25486cca76de31421cc6
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-03-01 19:35:55 +0800

    Deterministicly test random generate of meal availability

--- app/services/meal_assignment_service.rb
+++ app/services/meal_assignment_service.rb
@@ -4,6 +4,15 @@ class MealAssignmentService
   end
 
   def generate_meal_availability(no_of_users)
-    return MealAvailability.create!
+    meals_count = (no_of_users - 5) + rand(5 * 2)
+    availability = MealAvailability.create!(availability_for: Time.zone.now)
+
+    ['chicken', 'mutton', 'beef', 'fish', 'prawn', 'squid'].each do |attr|
+      meal = rand(meals_count)
+      availability.send("#{attr}=", meal)
+      meals_count -= meal
+    end
+
+    return availability
   end
 end
--- spec/services/meal_assignment_service_spec.rb
+++ spec/services/meal_assignment_service_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe 'MealAssignmentService' do
   describe 'generate random meal amount' do
     let(:service) { MealAssignmentService.new }
@@ -6,10 +8,54 @@ RSpec.describe 'MealAssignmentService' do
       expect { service.generate_meal_availability(0) }.to change { MealAvailability.count }.by(1)
     end
 
-    it 'generate a minimum number of meals which is the number of user minus 5' do
-      no_of_users = 10
-      meal_availability = service.generate_meal_availability(no_of_users)
-      expect meal_availability.total > 5
+    context 'it generate a random for each meal type' do
+      before do
+        srand(42)
+      end
+
+      it 'generate a minimum number of meals which is the number of user minus 5' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.total).to eq(10)
+      end
+
+      it 'generate 3 chicken' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.chicken).to eq(3)
+      end
+
+      it 'generate 4 mutton' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.mutton).to eq(4)
+      end
+
+      it 'generate 2 beef' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.beef).to eq(2)
+      end
+
+      it 'generate 0 fish' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.fish).to eq(0)
+      end
+
+      it 'generate 1 prawn' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.prawn).to eq(1)
+      end
+
+      it 'generate 0 squid' do
+        no_of_users = 10
+        meal_availability = service.generate_meal_availability(no_of_users)
+        expect(meal_availability.squid).to eq(0)
+      end
     end
+
+
   end
 end
